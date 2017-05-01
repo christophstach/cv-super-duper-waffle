1dd417c79d06f7bcf674b95aed727f99107a51bc
Christoph Stach <christoph.stach@gmail.com>
2017-05-01 12:22:36 +0200

    Added json-ld about me

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,12 @@ export default {
   head: {
     meta: [
       {
-        id: 'app-name',
+        id: 'meta-application-name',
         name: 'application-name',
         content: 'Website of Christoph Stach'
       },
       {
-        id: 'desc',
+        id: 'meta-description',
         name: 'description',
         content: 'I\'m a full stack web engineer who is studying at the university of applied sciences HTW Berlin. My focus is on developing single page frontend applications.'
       }
@@ -27,7 +27,21 @@ export default {
         type: 'application/ld+json',
         inner: JSON.stringify(
           {
-            '@Context': 'http://schema.org'
+            '@Context': 'http://schema.org',
+            '@Type': 'Person',
+            name: 'Christoph Stach',
+            jobTitle: 'Frontend Software Engineer',
+            height: '185 cm',
+            birthPlace: 'Düsseldorf, Germany',
+            gender: 'male',
+            nationality: 'German',
+            birthDate: new Date(1988, 2, 15),
+            url: 'http://christophstach.github.io',
+            sameAs: [
+              'https://github.com/christophstach',
+              'https://www.xing.com/profile/Christoph_Stach',
+              'https://de.linkedin.com/in/christoph-stach-7586b958'
+            ]
           }
         )
       }
