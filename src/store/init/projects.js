822895a5955d04d74c1cb65726b4d18a85a856d3
Christoph Stach <christoph.stach@gmail.com>
2017-06-26 10:24:16 +0200

    removed hint

--- src/store/init/projects.js
+++ src/store/init/projects.js
@@ -18,7 +18,6 @@ export default [
     image: 'Twitter_Logo_White_On_Blue.png',
     title: 'Data science with Twitter',
     description: 'A project we are currently doing at university. It shows different analyses about Twitter.',
-    hint: 'Currently displaying fake data',
     link: '/projects/twitter/',
     data: {
       fromDate: oneWeekAgo,
